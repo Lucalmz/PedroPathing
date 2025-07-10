d1a29b815e04fb05a5238894f3916c173a6c2fe3
LucaLmz <qumingquchangbuhuichong@outlook.com>
2025-07-10 23:03:49 +0800

    Extend keywords in Action.java

--- src/main/java/com/pedropathing/BotFactory/Action.java
+++ src/main/java/com/pedropathing/BotFactory/Action.java
@@ -1,5 +1,14 @@
 package com.pedropathing.BotFactory;
 
 public enum Action {
-    Open,Close,LaxClose,OpenMore,Up,Down,SlideOut,SlideIn,Rotate,Back,Center,Turned,Init,LiftUp,LiftDown,LeftTurned,RightTurned,PutDown,Set,Reset,Forward,Reverse,Backward
+    Open,Close,LaxClose,OpenMore,Shut,Fold,Seal,Narrow,
+    Up,Down,SlideOut,SlideIn,LiftUp,LiftDown,LeftTurned,RightTurned,Elevate,Ascend,Decened,Depress,
+    In,Out,PushIn,PullOut,Push,Pull,Retract,Extend,Left,Right,ShiftLeft,ShiftRight,Away,Towards,
+    Rotate,Back,Center,Turned,Spin,Twist,Revolve,Clockwise,CounterClockwise,Swivel,Pivot,
+    Init,Activate,Deactivate,Enable,Disable,
+    Lock,Unlock,Secure,Release,Anchor,
+    Launch,Home,Default,
+    Start,Stop,
+    PutDown,Set,Reset,
+    Forward,Reverse,Backward
 }
